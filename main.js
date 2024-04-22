ad921fd635e70cc354128c3525d72c7995e4de37
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-23 02:57:48 +0500

    addding todo

---
+++ main.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+// installing inquirer
+import inquirer from "inquirer";
+import chalk from "chalk";
+//making Array//
+let todoList = ["Nousheen", "Fatima", "Adeel"];
+//using function//
+async function createTodos(todoList) {
+    do {
+        let answer = await inquirer.prompt({
+            type: "list",
+            message: "Select an operation",
+            name: "Select",
+            choices: ["Add", "Update", "Veiw", "Remove", "Exit"],
+        });
+        if (answer.Select == "Add") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "todo",
+            });
+            todoList.push(addTodo.todo);
+            todoList.forEach(todo => console.log(todo));
+        }
+        if (answer.Select == "Update") {
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "Update items in the list",
+                name: "todo",
+                choices: todoList.map(item => item)
+            });
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "todo",
+            });
+            let newTodo = todoList.filter(val => val !== updateTodo.todo);
+            todoList = [...newTodo, addTodo.todo];
+            todoList.forEach(todo => console.log(todo));
+        }
+        if (answer.Select == "Veiw") {
+            console.log(chalk.bgCyanBright("***To Do List***"));
+            todoList.forEach(todo => console.log(chalk.magentaBright(todo)));
+            console.log(chalk.bgCyanBright("******************"));
+        }
+        if (answer.Select == "Remove") {
+            let removeTodo = await inquirer.prompt({
+                type: "list",
+                message: "Remove items in the list",
+                name: "todo",
+                choices: todoList.map(item => item)
+            });
+            let newTodo = todoList.filter(val => val !== removeTodo.todo);
+            todoList = [...newTodo];
+            todoList.forEach(todo => console.log(todo));
+        }
+        if (answer.Select == "Exit") {
+            console.log(chalk.greenBright("Thank you for using this application"));
+            console.log(chalk.redBright("Exiting...."));
+            break;
+        }
+    } while (true);
+}
+//calling function//
+createTodos(todoList);
